26e6dc19ed16d1fa04c81633cbcf375d23e685e0
CH-coding-fire <ebonynova8@gmail.com>
2024-12-29 19:46:47 +0800

    add env

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
+import dotenv from 'dotenv';
+dotenv.config();
 
-console.log('Hello, World, this is working');
+console.log('This is working, environment variable: ', process.env.VAR_FOR_TEST);
 
 //axios
 axios.get('https://jsonplaceholder.typicode.com/posts/1')
